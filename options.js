b70b8cbab60dafc99d7993d94cbb030e602d438e
Mark Hegedus <mark.hegedus@opensignal.com>
2025-02-12 16:09:01 +0100

    set storePath when using token grabber

--- options.js
+++ options.js
@@ -302,6 +302,11 @@ async function authButtonClick() {
 
 async function tokenGrabberClick() {
   const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  const storePath = document.getElementById('storeBox');
+  if (storePath.value.length > 0 && storePath.value[0] === '/') {
+    storePath.value = storePath.value.substring(1);
+  }
+  await browser.storage.sync.set({ storePath: storePath.value });
   for (let tabIndex = 0; tabIndex < tabs.length; tabIndex++) {
     const tab = tabs[tabIndex];
     if (tab.url) {
